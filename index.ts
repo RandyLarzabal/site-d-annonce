9e446dad2ca664ac3f8fd144b15b11ba23be7183
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2022-11-03 16:55:46 +0100

    feature<front>: update

--- index.ts
+++ index.ts
@@ -1,25 +1,27 @@
 import express from "express";
-import usersRouter from './user';
-import productsRouter from './produit';
+import usersRouter from "./user";
 const path = require("path");
 
 const app = express();
 const port = process.env.PORT || "8000";
 app.use(usersRouter);
-app.use(productsRouter);
-var http = require('http').createServer(app);
+var http = require("http").createServer(app);
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
-  });
+  console.log(`Listening at http://localhost:${port}/api`);
+});
 
-  app.get("/", (req, res) => {
-    res.render("index");
-  });
-    
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.get("/product", (req, res) => {
   res.render("product");
+});
+
+app.get("/add-product", (req, res) => {
+  res.render("add-product");
 });
