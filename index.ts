04ec12b49bbdbb3e5a1d15bf3a75857857f2c9df
thomasalzonne <thomas.alzonne@gmail.com>
2022-11-03 15:22:09 +0100

    update index.ts

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || "8000";
 
+app.set('view engine', 'pug')
+
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
   });
