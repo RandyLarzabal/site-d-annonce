b83f818e38fe570140f14a4dd6f37d7705c996fe
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2022-11-03 15:52:01 +0100

    fix<front>: update

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || "8000";
 
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
