08c77aff362a2abd4eb3b3896aee69a59458e1fa
thomasalzonne <thomas.alzonne@gmail.com>
2022-11-03 17:03:43 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import productRouter from './produit';
 import usersRouter from "./user";
+import db from './db'
 const path = require("path");
 
 const app = express();
 const port = process.env.PORT || "8000";
+app.use(productRouter)
 app.use(usersRouter);
 var http = require("http").createServer(app);
 
@@ -14,8 +17,15 @@ const server = http.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 
-app.get("/", (req, res) => {
-  res.render("index");
+
+
+app.get("/", (req, res) => { 
+    db('products')
+    .then((e) => {
+      if(e) {
+        res.render("index", {products : e});
+      }
+    });
 });
 
 app.get("/product", (req, res) => {
